777ce0df9814653347856961ebaf82fb6afb8c83
Alfonce Yano <ogongoyanojnr@gmail.com>
2025-12-23 15:46:52 +0300

    🍵🍵🍵 Second endpoint

--- TheSampleApi/EndPoints/CourseEndpoints.cs
+++ TheSampleApi/EndPoints/CourseEndpoints.cs
@@ -6,9 +6,17 @@ namespace TheSampleApi.EndPoints
     {
         public static void AddCourseEndpoints(this WebApplication app)
         {
-           app.MapGet("/courses", (CourseData data) => {
-                return Results.Ok(data.Courses);
-           });
+           app.MapGet("/courses", LoadAllCourses);
+           app.MapGet("/courses/{id}", LoadCoursesById);
+        }
+
+        private static IResult LoadAllCourses(CourseData data)
+        {
+            return Results.Ok(data.Courses);
+        }
+        private static IResult LoadCoursesById(CourseData data, int id)
+        {
+            return Results.Ok(data.Courses.SingleOrDefault(x=>x.Id==id));
         }
     }
 }
